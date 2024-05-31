a902ac5430fb33fd762f9a6883fa13e5c5408f29
SofiiaPol <sophia.polyakova05@gmail.com>
2024-06-01 01:14:09 +0300

    almost finish task

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,30 +1,36 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const text = document.querySelector('.ac-container');
-console.log(text);
-const accordion = new Accordion(text, {
-  onOpen: cur => {
-    console.log('open', cur);
-  },
-  onClose: cur => {
-    console.log('close', cur);
-  },
+import { FAQ_ITEMS } from './constants';
+
+const faqUlElement = document.querySelector('.ac-container');
+faqUlElement.innerHTML = getInnerHtml(FAQ_ITEMS);
+const accordion = new Accordion(faqUlElement, {
   triggerClass: 'arrow-btn',
-  activeClass: 'asked-question-item-open',
   duration: 400,
-  //   openOnInit: [1],
-  //   openOnInit: [0],
-  //   collapse: false,
+  openOnInit: [0],
 });
 console.log(accordion);
-accordion.closeAll();
 
-// document.addEventListener('DOMContentLoaded', function () {
-//   new Accordion('.asked-question-list', {
-//     duration: 400,
-//     openOnInit: [0],
-//     collapse: false,
-//     showMultiple: false,
-//   });
-// });
+function getInnerHtml(items) {
+  console.log(items);
+  const htmlString = items
+    .map(item => {
+      return `
+      <li class="asked-question-item ac">
+        <div class="question-header-container ac-header">
+          <h3 class="question">
+            ${item.question}
+          </h3>
+          <div class="arrow-btn">d</div>
+        </div>
+        <p class="question-answer-p ac-panel">
+          ${item.answer}
+        </p>
+      </li>
+      `;
+    })
+    .join('');
+  console.log(htmlString);
+  return htmlString;
+}
