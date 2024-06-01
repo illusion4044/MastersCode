6bae648e49e1708f68a665bc958987faddc466fb
OksanaGukova <kseniagukova@ukr.net>
2024-06-01 13:06:57 +0300

    reviews -js
    reviews -js

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -1,38 +1,29 @@
 
 
+
 function createMarkup(review) {
   return `<li class="swiper-slide">
-       <p class="review-text">${review.review}</p>
-        <div class="swiper-img-container"> <img class="reviews-img" src="${review.avatar_url}" alt="Avatar">
-        <h3 class="name">${review.author}</h3>
-      </div></li>`;
+    <p class="review-text">${review.review}</p>
+    <div class="swiper-img-container">
+      <img class="reviews-img" src="${review.avatar_url}" alt="Avatar">
+      <h3 class="name">${review.author}</h3>
+    </div>
+  </li>`;
 }
 
-function FetchMarkup(foo, murkUp) {
-  const reviewsList = document.querySelector('.reviews-list');
-  fetch('https://portfolio-js.b.goit.study/api/reviews')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .then(reviews => {
-      const markup = reviews
-        .map(review => {
-          return murkUp(review);
-        })
-        .join('');
-
-      reviewsList.innerHTML = markup;
-      foo();
-    })
-    .catch(error => {
-      console.error(error);
-      reviewsList.innerHTML = 'Not found';
-    });
+async function fetchReviews() {
+  try {
+    const response = await fetch(
+      'https://portfolio-js.b.goit.study/api/reviews'
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
 }
-
-
 
 
