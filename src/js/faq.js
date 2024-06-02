a290935c8102f77f446fe5fa6931ab11b7301d41
SofiiaPol <sophia.polyakova05@gmail.com>
2024-06-02 21:13:17 +0300

    finish task

--- src/js/faq.js
+++ src/js/faq.js
@@ -23,8 +23,8 @@ function getInnerHtml(items) {
             ${item.question}
           </h3>
           <div class="arrow-btn">
-            <svg class="accordion-svg" width="32" height="32">
-              <use href="../img/symbol-defs.svg#icon-down"></use>
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M18 9L12 15L6 9" stroke="#292929" stroke-width="2" stroke-linecap="round"/>
             </svg>
           </div>
         </div>
@@ -38,3 +38,15 @@ function getInnerHtml(items) {
   console.log(htmlString);
   return htmlString;
 }
+
+// function turnUp() {
+//   turnSvg(45);
+// }
+
+// function turnDown() {
+//   turnSvg(-45);
+// }
+
+// function turnSvg() {
+//   if
+// }
