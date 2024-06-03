1064444b7ea062bc9128481fe28d4c1c1eb8d0a6
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-06-03 15:37:20 +0300

    finished buttons

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -61,18 +61,18 @@ const createProjectMarkup = project => {
   return `
     <li class="projects-item">
       <picture>
-        <source srcset="./img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ./img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(min-width: 1280px)">
-          <source srcset="./img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ./img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(min-width: 768px)">
-            <source srcset="./img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ./img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(max-width: 767px)">
-        <img class="project-img" src="./img/my-projects_img/desk-${project.imageBase}-1x.jpg" alt="${project.title}">
+        <source srcset="../img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ../img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(min-width: 1280px)">
+          <source srcset="../img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ../img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(min-width: 768px)">
+            <source srcset="../img/my-projects_img/desk-${project.imageBase}-1x.jpg 1x, ../img/my-projects_img/desk-${project.imageBase}-2x.jpg 2x" media="(max-width: 767px)">
+        <img class="project-img" src="../img/my-projects_img/desk-${project.imageBase}-1x.jpg" alt="${project.title}">
       </picture>
       <p class="technologies">${project.technologies}</p>
       <div class="title-btn-wrapper">
         <p class="my-project-title"> ${project.title} </p>
       <button class="visit-btn" type="button" onclick="window.open('https://github.com/illusion4044/MastersCode', '_blank');">
           Visit
-          <svg class="visit-icon" version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
-            <path fill="none" stroke-linejoin="round" stroke-linecap="round" stroke-miterlimit="4" stroke-width="2.6667" stroke="#00b068" d="M6.333 25.667l19.333-19.333M25.667 6.333h-18M25.667 6.333v18"></path>
+          <svg class="visit-icon" width="24px" height="24px">
+          <use href="../img/symbol-defs.svg#icon-icon241"></use>
           </svg>
         </button>
       </div>
