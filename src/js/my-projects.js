2d3d3ca008fc010dcc864a42b9a2110ec01976a8
PetroGal <69636047+PetroGal@users.noreply.github.com>
2024-06-05 05:40:42 +0300

    adds 2x images

--- src/js/my-projects.js
+++ src/js/my-projects.js
@@ -1,63 +1,148 @@
-import myprojects1 from '../img/my-projects_img/desk-myprojects1-1x.jpg';
-import myprojects2 from '../img/my-projects_img/desk-myprojects2-1x.jpg';
-import myprojects3 from '../img/my-projects_img/desk-myprojects3-1x.jpg';
-import myprojects4 from '../img/my-projects_img/desk-myprojects4-1x.jpg';
-import myprojects5 from '../img/my-projects_img/desk-myprojects5-1x.jpg';
-import myprojects6 from '../img/my-projects_img/desk-myprojects6-1x.jpg';
-import myprojects7 from '../img/my-projects_img/desk-myprojects7-1x.jpg';
-import myprojects8 from '../img/my-projects_img/desk-myprojects8-1x.jpg';
-import myprojects9 from '../img/my-projects_img/desk-myprojects9-1x.jpg';
-import myprojects10 from '../img/my-projects_img/desk-myprojects10-1x.jpg';
+// import myprojects1 from '../img/my-projects_img/desk-myprojects1-1x.jpg';
+// import myprojects2 from '../img/my-projects_img/desk-myprojects2-1x.jpg';
+// import myprojects3 from '../img/my-projects_img/desk-myprojects3-1x.jpg';
+// import myprojects4 from '../img/my-projects_img/desk-myprojects4-1x.jpg';
+// import myprojects5 from '../img/my-projects_img/desk-myprojects5-1x.jpg';
+// import myprojects6 from '../img/my-projects_img/desk-myprojects6-1x.jpg';
+// import myprojects7 from '../img/my-projects_img/desk-myprojects7-1x.jpg';
+// import myprojects8 from '../img/my-projects_img/desk-myprojects8-1x.jpg';
+// import myprojects9 from '../img/my-projects_img/desk-myprojects9-1x.jpg';
+// import myprojects10 from '../img/my-projects_img/desk-myprojects10-1x.jpg';
+// import icon from '../img/SVG/icon241.svg';
+
+// const projects = [
+//   {
+//     imageBase: myprojects1,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Power pulse webservice ',
+//   },
+//   {
+//     imageBase: myprojects2,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Mimino website',
+//   },
+//   {
+//     imageBase: myprojects3,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Vyshyvanka vibes Landing Page',
+//   },
+//   {
+//     imageBase: myprojects4,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Green harvest online store',
+//   },
+//   {
+//     imageBase: myprojects5,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Wallet webservice',
+//   },
+//   {
+//     imageBase: myprojects6,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Chego jewelry website',
+//   },
+//   {
+//     imageBase: myprojects7,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Energy flow webservice ',
+//   },
+//   {
+//     imageBase: myprojects8,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Fruitbox online store',
+//   },
+//   {
+//     imageBase: myprojects9,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'English excellence webservice',
+//   },
+//   {
+//     imageBase: myprojects10,
+//     technologies: 'React, JavaScript, Node JS, Git',
+//     title: 'Starlight studio landing page',
+//   },
+// ];
+
+import myprojects1_1x from '../img/my-projects_img/desk-myprojects1_1x.jpg';
+import myprojects2_1x from '../img/my-projects_img/desk-myprojects2_1x.jpg';
+import myprojects3_1x from '../img/my-projects_img/desk-myprojects3_1x.jpg';
+import myprojects4_1x from '../img/my-projects_img/desk-myprojects4_1x.jpg';
+import myprojects5_1x from '../img/my-projects_img/desk-myprojects5_1x.jpg';
+import myprojects6_1x from '../img/my-projects_img/desk-myprojects6_1x.jpg';
+import myprojects7_1x from '../img/my-projects_img/desk-myprojects7_1x.jpg';
+import myprojects8_1x from '../img/my-projects_img/desk-myprojects8_1x.jpg';
+import myprojects9_1x from '../img/my-projects_img/desk-myprojects9_1x.jpg';
+import myprojects10_1x from '../img/my-projects_img/desk-myprojects10_1x.jpg';
+import myprojects1_2x from '../img/my-projects_img/desk-myprojects1_2x.jpg';
+import myprojects3_2x from '../img/my-projects_img/desk-myprojects3_2x.jpg';
+import myprojects2_2x from '../img/my-projects_img/desk-myprojects2_2x.jpg';
+import myprojects5_2x from '../img/my-projects_img/desk-myprojects5_2x.jpg';
+import myprojects4_2x from '../img/my-projects_img/desk-myprojects4_2x.jpg';
+import myprojects6_2x from '../img/my-projects_img/desk-myprojects6_2x.jpg';
+import myprojects7_2x from '../img/my-projects_img/desk-myprojects7_2x.jpg';
+import myprojects8_2x from '../img/my-projects_img/desk-myprojects8_2x.jpg';
+import myprojects9_2x from '../img/my-projects_img/desk-myprojects9_2x.jpg';
+import myprojects10_2x from '../img/my-projects_img/desk-myprojects10_2x.jpg';
 import icon from '../img/SVG/icon241.svg';
 
 const projects = [
   {
-    imageBase: myprojects1,
+    imageBase_1x: myprojects1_1x,
+    imageBase_2x: myprojects1_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Power pulse webservice ',
   },
   {
-    imageBase: myprojects2,
+    imageBase_1x: myprojects2_1x,
+    imageBase_2x: myprojects2_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Mimino website',
   },
   {
-    imageBase: myprojects3,
+    imageBase_1x: myprojects3_1x,
+    imageBase_2x: myprojects3_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Vyshyvanka vibes Landing Page',
   },
   {
-    imageBase: myprojects4,
+    imageBase_1x: myprojects4_1x,
+    imageBase_2x: myprojects4_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Green harvest online store',
   },
   {
-    imageBase: myprojects5,
+    imageBase_1x: myprojects5_1x,
+    imageBase_2x: myprojects5_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Wallet webservice',
   },
   {
-    imageBase: myprojects6,
+    imageBase_1x: myprojects6_1x,
+    imageBase_2x: myprojects6_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Chego jewelry website',
   },
   {
-    imageBase: myprojects7,
+    imageBase_1x: myprojects7_1x,
+    imageBase_2x: myprojects7_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Energy flow webservice ',
   },
   {
-    imageBase: myprojects8,
+    imageBase_1x: myprojects8_1x,
+    imageBase_2x: myprojects8_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Fruitbox online store',
   },
   {
-    imageBase: myprojects9,
+    imageBase_1x: myprojects9_1x,
+    imageBase_2x: myprojects9_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'English excellence webservice',
   },
   {
-    imageBase: myprojects10,
+    imageBase_1x: myprojects10_1x,
+    imageBase_2x: myprojects10_2x,
     technologies: 'React, JavaScript, Node JS, Git',
     title: 'Starlight studio landing page',
   },
@@ -69,25 +154,15 @@ const moreBtnEl = document.querySelector('.more-btn');
 let currentIndex = 0;
 const projectsPerPage = 3;
 
+//desk-myprojects1-1x
 const createProjectMarkup = project => {
   return `
 <li class="projects-item">
       <picture>
-        <source srcset="${project.imageBase} 1x, ${project.imageBase.replace(
-    '-1x',
-    '-2x'
-  )} 2x" media="(min-width: 1280px)">
-        <source srcset="${project.imageBase} 1x, ${project.imageBase.replace(
-    '-1x',
-    '-2x'
-  )} 2x" media="(min-width: 768px)">
-        <source srcset="${project.imageBase} 1x, ${project.imageBase.replace(
-    '-1x',
-    '-2x'
-  )} 2x" media="(max-width: 767px)">
-        <img class="project-img" src="${project.imageBase}" alt="${
-    project.title
-  }">
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(min-width: 1280px)">
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(min-width: 768px)">
+        <source srcset="${project.imageBase_1x} 1x, ${project.imageBase_2x} 2x" media="(max-width: 767px)">
+        <img class="project-img" src="${project.imageBase_1x}" alt="${project.title}">
       </picture>
       <p class="technologies">${project.technologies}</p>
       <div class="title-btn-wrapper">
